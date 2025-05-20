ba4ffeb886f2733fd114f9e4cb894272599f75c7
Ismaël Sebbane <ismael.sebbane@gmail.com>
2025-05-20 19:30:31 +0200

    feat: added sidebarprovider in layout

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import stylesheet from "./app.css?url";
+import { SidebarProvider } from "./components/ui/sidebar";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -34,7 +35,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <SidebarProvider>
+          {children}
+        </SidebarProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
